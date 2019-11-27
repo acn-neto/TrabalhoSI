f1f66d9595a952a63a5e40fd289ffb732883a016
knetiin <netiin.carlos.nic@hotmail.com>
2019-11-27 16:43:57 -0300

    travando na base 64

--- MSN-backend-66/index.js
+++ MSN-backend-66/index.js
@@ -1,19 +1,64 @@
-const express = require('express')
-const cors = require('cors')
+// SERVER AND REQUEST PROPS
+const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const openssl = require('openssl-nodejs');
+
+// REQUEST TO SERVER
 const axios = require('axios');
 
+// CRYPTOGRAPHY
+const openssl = require('openssl-nodejs');
+
+// READ AND WRITE IN FILES
+var fs = require("fs");
+
+// HASH
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// SERVER PREPARATIONS
 var app = express()
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var myChat = {}
+// APLICATION PREPARATIONS
+var myChat = JSON.parse(fs.readFileSync("./db.json", "utf8"))
+
+if (!myChat.certificate) {
+  // CREATE PRIVATE KEY
+  openssl(['genrsa', '-out', 'myPK.pem'], function (err) {
+    console.log('CREATE PK')
+    console.log(err.toString())
+
+    // CREATE PUBLIC KEY
+    openssl(['rsa', '-in', 'myPK.pem', '-pubout', '-out', 'myPU.pem'], function (err) {
+      console.log('CREATE PU')
+      console.log(err.toString())
+    })
+  })
+  myChat.certificate = true
+  fs.writeFileSync("./db.json", JSON.stringify(myChat), "utf8")
+}
+
+// GET THE AC CERTIFICATE
+axios.get('http://localhost:8000/certificate')
+.then(function(response) {
+  fs.writeFileSync("./openssl/ac-pukey.pem", response.data, "utf8")
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+
+
+/***************************************************************** APIs *****************************************************************/
 
 // LOGIN
 app.post('/chat/login', function (req, res) {
+  let extra = ''
+
   if (myChat.name) {
     let myName = req.body.name.replace(/ /g,'').toLowerCase()
     let chatName = myChat.name.replace(/ /g,'').toLowerCase()
@@ -27,11 +72,15 @@ app.post('/chat/login', function (req, res) {
     myChat.name = req.body.name
     myChat.avatar = req.body.avatar
     myChat.messages = {}
+    fs.writeFileSync("./db.json", JSON.stringify(myChat), "utf8")
+
+    extra = fs.readFileSync("./openssl/myPU.pem", "utf8")
   }
   
   axios.post('http://localhost:8000/chat/login', {
       name: req.body.name,
-      avatar: req.body.avatar
+      avatar: req.body.avatar,
+      extra: extra
     })
     .then(response => {
       res.send(req.body)
@@ -92,18 +141,43 @@ app.post('/chat/message', function (req, res) {
     res.send('WRONG BACKEND')
   }
 
-  axios.post('http://localhost:8000/chat/message', {
-      myName: myName,
-      yourName: yourName,
-      message: message
-    })
-    .then(response => {
-      res.send(req.body)
-    })
-    .catch(error => {
-      res.status(400)
-      res.send('ERRO AO CONECTAR AO SERVER')
-    })
+  let messageJson = JSON.stringify({
+    myName: myName,
+    yourName: yourName,
+    message: message
+  })
+
+  // TESTE DE INTEGRIDADE
+  let FAKEmessageJson = JSON.stringify({
+    myName: myName,
+    yourName: yourName,
+    message: message + 'BATATA'
+  })
+
+  var messageHash = bcrypt.hashSync(messageJson, saltRounds)
+
+  var bufferHash = Buffer.from(messageHash)
+  openssl(['rsautl', '-sign', '-inkey', 'myPK.pem', '-in',  { name:'hash.txt', buffer: bufferHash }, '-out', 'hashAssinado.txt'], function (err) {
+    console.log('SIGNING THE HASH')
+    console.log(err.toString())
+
+    var hashSigned = fs.readFileSync("./openssl/hashAssinado.txt", "utf8")
+
+    let myBody = {
+      // TESTE DE INTEGRIDADE ****************************************************************************************
+      // messageJson: FAKEmessageJson,
+      messageJson: messageJson,
+      messageHash: hashSigned
+    }
+    axios.post('http://localhost:8000/chat/message', myBody)
+      .then(response => {
+        res.send(req.body)
+      })
+      .catch(error => {
+        res.status(400)
+        res.send('ERRO AO CONECTAR AO SERVER')
+      })
+  })
 })
 
 // GET ALL MESSAGES
@@ -144,6 +218,7 @@ app.get('/chat/message', function (req, res) {
   axios.get(`http://localhost:8000/chat/messages?myName=${myName}&yourName=${yourName}`)
     .then(response => {
       myChat.messages[yourName] = response.data
+      fs.writeFileSync("./db.json", JSON.stringify(myChat), "utf8")
       let messages = myChat.messages[yourName].filter((el, i) => {
         return i >= index
       })
--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,61 @@
-const express = require('express')
-const cors = require('cors')
+// SERVER AND REQUEST PROPS
+const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+// CRYPTOGRAPHY
 const openssl = require('openssl-nodejs');
 
+// READ AND WRITE IN FILES
+var fs = require("fs");
+
+// HASH
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// SERVER PREPARATIONS
 var app = express()
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var chat = {}
+// APLICATION PREPARATIONS
+var chat = JSON.parse(fs.readFileSync("./db.json", "utf8"))
+var online = JSON.parse(fs.readFileSync("./online.json", "utf8"))
+var offline = JSON.parse(fs.readFileSync("./offline.json", "utf8"))
 
-var online = [
-]
-var offline = [
-]
+if (!chat.certificate) {
+  // CREATE PRIVATE KEY
+  openssl(['genrsa', '-out', 'myPK.pem'], function (err) {
+    console.log('CREATE PK')
+    console.log(err.toString())
+
+    // CREATE PUBLIC KEY
+    openssl(['rsa', '-in', 'myPK.pem', '-pubout', '-out', 'myPU.pem'], function (err) {
+      console.log('CREATE PU')
+      console.log(err.toString())
+    })
+  })
+  chat.certificate = true
+  fs.writeFileSync("./db.json", JSON.stringify(chat), "utf8")
+}
+
+/***************************************************************** APIs *****************************************************************/
+
+// SEND AC CERTIFICATE
+app.get('/certificate', function (req, res) {
+  res.download('./openssl/myPU.pem', 'ac-pukey.pem')
+})
 
 // LOGIN
 app.post('/chat/login', function (req, res) {
   let name = req.body.name
   let id = name.replace(/ /g,'').toLowerCase()
   let avatar = req.body.avatar
+  let extra = req.body.extra
 
+  // organiza as pessoas onlines e offlines
   if(!online.find(el => {
     return el.name.replace(/ /g,'').toLowerCase() === name.replace(/ /g,'').toLowerCase()
   })) {
@@ -35,6 +69,7 @@ app.post('/chat/login', function (req, res) {
     return el.name.replace(/ /g,'').toLowerCase() !== name.replace(/ /g,'').toLowerCase()
   })
 
+  // atualiza os dados ou caso necessário cria
   if (chat[id]) {
     chat[id].name = name
     chat[id].avatar = avatar
@@ -44,7 +79,15 @@ app.post('/chat/login', function (req, res) {
       avatar: avatar,
       messages: {}
     }
+    if (extra) {
+      fs.writeFileSync(`./openssl/${id}-pu.pem`, extra, "utf8")
+    }
   }
+
+  // grava todas mudanças
+  fs.writeFileSync("./db.json", JSON.stringify(chat), "utf8")
+  fs.writeFileSync("./online.json", JSON.stringify(online), "utf8")
+  fs.writeFileSync("./offline.json", JSON.stringify(offline), "utf8")
 
   res.send(req.body)
 })
@@ -52,9 +95,9 @@ app.post('/chat/login', function (req, res) {
 // LOGOUT
 app.post('/chat/logout', function (req, res) {
   let name = req.body.name
-  let id = name.replace(/ /g,'').toLowerCase()
   let avatar = req.body.avatar
 
+  // organiza as pessoas onlines e offlines
   if(!offline.find(el => {
     return el.name.replace(/ /g,'').toLowerCase() === name.replace(/ /g,'').toLowerCase()
   })) {
@@ -68,6 +111,10 @@ app.post('/chat/logout', function (req, res) {
     return el.name.replace(/ /g,'').toLowerCase() !== name.replace(/ /g,'').toLowerCase()
   })
 
+  // grava todas mudanças
+  fs.writeFileSync("./online.json", JSON.stringify(online), "utf8")
+  fs.writeFileSync("./offline.json", JSON.stringify(offline), "utf8")
+
   res.send(req.body)
 })
 
@@ -79,6 +126,49 @@ app.get('/chat/people', function (req, res) {
   }
   res.status(200)
   res.send(people)
+})
+
+// SENT MESSAGE
+app.post('/chat/message', function (req, res) {
+  let messageJson = req.body.messageJson
+  let hashSigned = req.body.messageHash
+
+  fs.writeFileSync("./openssl/hashSigned.txt", hashSigned, "utf8")
+  openssl(['rsautl', '-verify', '-inkey', 'teste.pem', '-pubin', '-in', 'hashSigned.txt', '-out', 'hashDecypher.txt'], function (err) {
+    console.log('DECYPHER THE HASH SIGNED')
+    console.log(err.toString())
+
+    var messageHash = fs.readFileSync("./openssl/hashDecypher.txt", "utf8")
+
+    if(!bcrypt.compareSync(messageJson, messageHash)) {
+      res.status(401)
+      console.log('INTEGRIDADE COMPROMETIDA')
+      res.send('INTEGRIDADE COMPROMETIDA')
+    } else {
+      let body = JSON.parse(messageJson)
+  
+      let myName = body.myName.replace(/ /g,'').toLowerCase()
+      let yourName = body.yourName.replace(/ /g,'').toLowerCase()
+      let message = body.message
+    
+      if (!chat[myName] || !chat[yourName]) {
+        res.status(400)
+        res.send('EMISSOR OU RECEPTOR INVALIDO')
+      }
+    
+      if (!chat[myName].messages[yourName]) {
+        chat[myName].messages[yourName] = []
+        chat[yourName].messages[myName] = []
+      }
+    
+      chat[myName].messages[yourName].push({ sent: true, message: message })
+      chat[yourName].messages[myName].push({ sent: false, message: message })
+    
+      fs.writeFileSync("./db.json", JSON.stringify(chat), "utf8")
+    
+      res.send(req.body)
+    }
+  })
 })
 
 // GET ALL MESSAGES
@@ -98,28 +188,6 @@ app.get('/chat/messages', function (req, res) {
 
   let messages = chat[myName].messages[yourName]
   res.send(messages)
-})
-
-// SENT MESSAGE
-app.post('/chat/message', function (req, res) {
-  let myName = req.body.myName.replace(/ /g,'').toLowerCase()
-  let yourName = req.body.yourName.replace(/ /g,'').toLowerCase()
-  let message = req.body.message
-
-  if (!chat[myName] || !chat[yourName]) {
-    res.status(400)
-    res.send('EMISSOR OU RECEPTOR INVALIDO')
-  }
-
-  if (!chat[myName].messages[yourName]) {
-    chat[myName].messages[yourName] = []
-    chat[yourName].messages[myName] = []
-  }
-
-  chat[myName].messages[yourName].push({ sent: true, message: message })
-  chat[yourName].messages[myName].push({ sent: false, message: message })
-
-  res.send(req.body)
 })
 
 // RODA O SERVIDOR
